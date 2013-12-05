976b28813a2819185ac3086a86aa8a93ca63b85d
Emily Evans <emilyevans91@gmail.com>
2013-12-05 16:53:27 +0000

    User controller and model updated to use bcrypt gem.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,13 +25,12 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
+    @user.password == params[:password]
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render action: 'show', status: :created, location: @user }
       else
-        format.html { render action: 'new' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -42,10 +41,8 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { head :no_content }
       else
-        format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
@@ -56,8 +53,16 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url }
       format.json { head :no_content }
+    end
+  end
+
+  def login
+    @user = User.find_by_email(params[:email])
+    if @user.password == params[:password]
+      give_token
+    else
+      #return json
     end
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,17 @@
+require 'bcrypt'
 class User < ActiveRecord::Base
+  include 'bcrypt'
   has_one :person, :dependent => destroy
+
+  # Method from http://bcrypt-ruby.rubyforge.org/
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  # Method from http://bcrypt-ruby.rubyforge.org/
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end
